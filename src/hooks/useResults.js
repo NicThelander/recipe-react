f6fea494b410db94f6d44ede570a0359e3cc5b9c
learningConstantly <nic.thelander@gmail.com>
2021-05-10 13:17:39 +0200

    cleaning up code

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -9,8 +9,7 @@ export default () => {
   const API_KEY2 = 'apiKey=ec28f651e0dd40658b6185cf99543a83';
   const API_KEY3 = 'apiKey=8f025297f1454a169b5c66c3d46a2ab6';
   const searchApi = async (searchTerm) => {
-    // offsetVal = offset;
-    searchVal = `complexSearch?${API_KEY2}&query=${searchTerm}&number=8`;
+    searchVal = `complexSearch?${API_KEY3}&query=${searchTerm}&number=8`;
     try {
       const response = await spoonacular.get(searchVal);
       setResults(response.data);
